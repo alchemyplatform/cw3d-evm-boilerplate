29bd8db3041c3eead6ea445142b17ea336bbe7c9
eversmile12 <iwasvitto@gmail.com>
2023-02-25 10:59:04 -0700

    new navbar

--- frontend/components/navigation/navbar.jsx
+++ frontend/components/navigation/navbar.jsx
@@ -4,10 +4,7 @@ export default function Navbar() {
 	return (
 		<nav className={styles.navbar}>
 			<a href="https://alchemy.com/?a=create-web3-dapp" target={"_blank"}>
-				<img
-					className={styles.alchemy_logo}
-					src="/alchemy_logo.svg"
-				></img>
+				<img className={styles.alchemy_logo} src="/cw3d-logo.png"></img>
 			</a>
 			<ConnectButton></ConnectButton>
 		</nav>
